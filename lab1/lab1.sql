d3c25d735cf46cd29eda6fdd6d37c1d6f413a748
Sergei Leshchenko <serezha.leshchenko@gmail.com>
2023-02-23 01:26:32 +0300

    Add input on the client side

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -1,3 +1,5 @@
+\prompt 'Enter table name: ' table_name
+SET val.name = :'table_name';
 do
 $$
     declare
@@ -8,32 +10,36 @@ $$
         type text;
         id_type oid;
         constr_name text;
-	constr_check text;
-	constr text;
+	  constr_check text;
+	  constr text;
         result text;
     begin
-        raise notice 'Таблица: %', 'Н_ЛЮДИ';
-        raise notice 'No.  Имя столбца         Атрибуты';
-        raise notice '--- ------------ ------------------------------------------------------';
-        select oid into id_table from pg_class where relname = 'Н_ЛЮДИ';
-        for columns in select * from pg_attribute where attrelid = id_table
-        loop
-	    if columns.attnum < 0 then
-	        continue;
-	    end if;
-            number = columns.attnum;
-            name = columns.attname;
-            id_type = columns.atttypid;
+        select oid into id_table from pg_class where relname = current_setting('val.name');
+	  if id_table is NULL then
+	      raise notice 'This table does not exist';
+	  else
+	      raise notice 'Таблица: %', current_setting('val.name');
+            raise notice 'No.  Имя столбца         Атрибуты';
+            raise notice '--- ------------ ------------------------------------------------------';
+            for columns in select * from pg_attribute where attrelid = id_table
+            loop
+	          if columns.attnum < 0 then
+	              continue;
+	          end if;
+                number = columns.attnum;
+                name = columns.attname;
+                id_type = columns.atttypid;
 	   
-            select typname into type from pg_catalog.pg_type where oid = id_type;
-	    select conname into constr_name from pg_constraint where oid = id_table;
-	    select * into constr_check from pg_get_constraintdef(id_table);
-	    constr = constr_name || ' ' || constr_check;
+                select typname into type from pg_catalog.pg_type where oid = id_type;
+	          select conname into constr_name from pg_constraint where oid = id_table;
+	          select * into constr_check from pg_get_constraintdef(id_table);
+	          constr = constr_name || ' ' || constr_check;
 
-            select format('%-3s %-16s %-6s %s', number, name, 'Type   :', type) into result;
-            raise notice '%', result;
-	    select format('%-20s %-8s %s', '|', 'Constr :', constr) into result;
-	    raise notice '%', result;
-        end loop;
-    end;
+                select format('%-3s %-16s %-6s %s', number, name, 'Type   :', type) into result;
+                raise notice '%', result;
+	          select format('%-20s %-8s %s', '|', 'Constr :', constr) into result;
+	          raise notice '%', result;
+            end loop;
+	    end if;
+        end;
 $$ LANGUAGE plpgsql;
