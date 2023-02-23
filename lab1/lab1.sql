b516a9ed80af5f036fd7c4416605de4d7a218451
Sergei Leshchenko <serezha.leshchenko@gmail.com>
2023-02-23 03:26:39 +0300

    Add constraint description for tables

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -10,35 +10,39 @@ $$
         type text;
         id_type oid;
         constr_name text;
-	  constr_check text;
-	  constr text;
+	constr_check text;
+	constr_oid oid;
+	constr_conkey text;
+	constr text;
         result text;
     begin
         select oid into id_table from pg_class where relname = current_setting('val.name');
-	  if id_table is NULL then
-	      raise notice 'This table does not exist';
-	  else
-	      raise notice 'Таблица: %', current_setting('val.name');
+	if id_table is NULL then
+	    raise notice 'Данная таблица не существует';
+	else
+	    raise notice 'Таблица: %', current_setting('val.name');
             raise notice 'No.  Имя столбца         Атрибуты';
             raise notice '--- ------------ ------------------------------------------------------';
             for columns in select * from pg_attribute where attrelid = id_table
             loop
-	          if columns.attnum < 0 then
-	              continue;
-	          end if;
+	        if columns.attnum < 0 then
+	            continue;
+	        end if;
                 number = columns.attnum;
                 name = columns.attname;
                 id_type = columns.atttypid;
 	   
                 select typname into type from pg_catalog.pg_type where oid = id_type;
-	          select conname into constr_name from pg_constraint where oid = id_table;
-	          select * into constr_check from pg_get_constraintdef(id_table);
-	          constr = constr_name || ' ' || constr_check;
-
+		constr_conkey = '{' || number || '}';
+	        select conname into constr_name from pg_constraint where conrelid = id_table;
+		select oid into constr_oid from pg_constraint where conrelid = id_table and constr_conkey = conkey::text and contype = 'c';
+	        select * into constr_check from pg_get_constraintdef(constr_oid);
+	        constr = constr_name || ' ' || constr_check;
+				
                 select format('%-3s %-16s %-6s %s', number, name, 'Type   :', type) into result;
                 raise notice '%', result;
-	          select format('%-20s %-8s %s', '|', 'Constr :', constr) into result;
-	          raise notice '%', result;
+	        select format('%-20s %-8s %s', '|', 'Constr :', constr) into result;
+	        raise notice '%', result;
             end loop;
 	    end if;
         end;
